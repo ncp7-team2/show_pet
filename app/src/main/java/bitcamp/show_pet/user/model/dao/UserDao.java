3a73e0f28632d5aa1bff452b82daf3d8b5ae2b23
lljh1992 <lljh1992@naver.com>
2023-09-16 15:05:14 +0900

    docs : UserDao lf 변경
    - UserDao crlf -> lf로 변경

--- app/src/main/java/bitcamp/show_pet/user/model/dao/UserDao.java
+++ app/src/main/java/bitcamp/show_pet/user/model/dao/UserDao.java
@@ -1,1 +1,14 @@
-package bitcamp.show_pet.user.model.dao;import bitcamp.show_pet.user.model.vo.User;import org.apache.ibatis.annotations.Mapper;import org.apache.ibatis.annotations.Param;@Mapperpublic interface UserDao {    User findByEmailAndPassword(            @Param("email") String email,            @Param("password") String password);}
+package bitcamp.show_pet.user.model.dao;
+
+import bitcamp.show_pet.user.model.vo.User;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+@Mapper
+public interface UserDao {
+
+    User findByEmailAndPassword(
+            @Param("email") String email,
+            @Param("password") String password);
+}
+              
