d6c6e49fa4769e7a54a8f9ae15293c2a3d8f59ba
wooni <jaeun1212@gmail.com>
2023-09-19 15:16:06 +0900

    fix : application.properties 수정

--- app/src/main/java/bitcamp/show_pet/App.java
+++ app/src/main/java/bitcamp/show_pet/App.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.util.UrlPathHelper;
@@ -20,6 +21,7 @@ public class App implements WebMvcConfigurer {
     }
 
     @GetMapping("/")
+//    @ResponseBody
     public String hello() {
         return "index";
     }
